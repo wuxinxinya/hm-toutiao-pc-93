db9e5f36c353e408f71116a2d96c0b9391e0543b
wuxinxinya <1252857980@qq.com>
2020-02-06 17:15:57 +0800

    登录模块  绘制表单完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,13 +3,38 @@
     <!-- 卡片 -->
     <el-card class='my-card'>
       <img src='../../assets/logo_index.png'>
+      <!-- 表单 -->
+      <el-form :model='loginForm' >
+        <!-- 表单项容器 -->
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐藏条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+         <el-button style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app-login'
+  name: 'app-login',
+  data(){
+    return{
+      loginForm:{
+        mobile:'',
+        code:''
+      }
+    }
+  }
 }
 </script>
 
