f22b716357518c8e844a0b9e191741f3dc51931d
wuxinxinya <1252857980@qq.com>
2020-02-07 17:51:32 +0800

    首页模块--404页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
@@ -20,8 +21,11 @@ const router = new VueRouter({
         path: '/',
         component: Welcome
       }]
-
-
+    },
+    // 通配规则 以上规则不符合的时候，走这个规则
+    {
+      path: '*',
+      component: NotFound
     }
   ] // 路由规则
 })
