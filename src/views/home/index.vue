ba987ebd9f1c9866b9c47f9552f62718c2e62364
wuxinxinya <1252857980@qq.com>
2020-02-07 11:55:35 +0800

    首页模块  基础布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="container-home">Home</div>
+    <el-container class="container-home">
+        <el-aside class="my-aside"
+                  width="200px">Aside</el-aside>
+        <el-container>
+            <el-header class="my-header">Header</el-header>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -9,4 +16,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.container-home {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .my-aside {
+    background: #002233;
+  }
+  .my-header {
+    border-bottom: 1px solid #ddd;
+  }
+}
 </style>
+
