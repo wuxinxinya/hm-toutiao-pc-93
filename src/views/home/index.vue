7fdae830c4155905f8571f6d3dc01c26c0294dc6
wuxinxinya <1252857980@qq.com>
2020-02-07 12:23:20 +0800

    首页模块--头部导航完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,27 @@
         <el-aside class="my-aside"
                   width="200px">Aside</el-aside>
         <el-container>
-            <el-header class="my-header">Header</el-header>
+            <el-header class="my-header">
+                <span class="icon el-icon-s-fold"></span>
+                <span class="text">江苏传智博客教育有限公司</span>
+                <!-- 右边下拉菜单 -->
+                <el-dropdown class="my-dropdown">
+                    <span class="el-dropdown-link">
+                        <!-- 用户头像 -->
+                        <img class="head"
+                             src="../../assets/avatar.jpg"
+                             alt="">
+                        <!-- 用户名称 -->
+                        <strong class="name">周杰伦</strong>
+                        <i class="el-icon-arrow-down el-icon--right"></i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>个人设置</el-dropdown-item>
+                        <el-dropdown-item>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+
+            </el-header>
             <el-main>Main</el-main>
         </el-container>
     </el-container>
@@ -27,6 +47,27 @@ export default {
   }
   .my-header {
     border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+    .my-dropdown {
+      float: right;
+      .head {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .name {
+        padding-left: 5px;
+        vertical-align: middle;
+      }
+    }
   }
 }
 </style>
